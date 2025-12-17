31c915313ed2d2284bd346b56a7a714a44bf1200
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-17 20:31:55 -0300

    CineVault App

---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+
+const API_KEY = "8818a07d44f658d054e713fddd51c641"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`)
+    const data = await response.json()
+    return data.results
+}
