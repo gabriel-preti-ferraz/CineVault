103ce1a450c3f36d26c2c57027861f0e3760f2f6
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-20 14:43:38 -0300

    feat: added .env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 
-const API_KEY = ""
 const BASE_URL = "https://api.themoviedb.org/3"
 
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${import.meta.env.VITE_API_KEY}`)
     const data = await response.json()
     return data.results
 }
